3cdd1cb7b7a1fe0bf42d02dc752ac6fa3ce8a1f0
Amy Lin <amy.lin@wepay.com>
2015-10-20 14:45:28 -0700

    Add view controller, update storyboard, connect views and gestures

--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -9,10 +9,60 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // Connects Interface Builder to the views
+    @IBOutlet var totalTextField: UITextField!
+    @IBOutlet var taxPctSlider: UISlider!
+    @IBOutlet var taxPctLabel: UILabel!
+    @IBOutlet var resultsTextView: UITextView!
+    
+    @IBAction func calculateTapped(sender: AnyObject) {
+        // Convert string to double
+        tipCalc.total = Double((totalTextField.text! as NSString).doubleValue)
+        
+        // Returns dictionary of possible tip percentages mapped to tip values
+        let possibleTips = tipCalc.returnPossibleTips()
+        var results = ""
+        
+        // Enumerate through both keys and values of dictionary
+        // Then build a string and add to the results text field
+        for (tipPct, tipValue) in possibleTips {
+            results += "\(tipPct)%: \(tipValue)\n"
+        }
+        
+        // Set the results text to the string
+        resultsTextView.text = results
+    }
+    
+    @IBAction func taxPercentageChanged(sender: AnyObject) {
+        tipCalc.taxPct = Double(taxPctSlider.value) / 100.0
+        refreshUI()
+    }
+    
+    @IBAction func viewTapped(sender: AnyObject) {
+        totalTextField.resignFirstResponder()
+    }
+    
+    
+    let tipCalc = TipCalculatorModel(total: 33.25, taxPct: 0.06)
+    
+    func refreshUI() {
+        // Convert from double to string
+        totalTextField.text = String(format: "%0.2f", tipCalc.total)
+        
+        // Mulitply by 100 to display as integer instead of decimal
+        taxPctSlider.value = Float(tipCalc.taxPct) * 100.0
+        
+        // Update label based on tax percentage
+        taxPctLabel.text = "Tax Percentage (\(Int(taxPctSlider.value))%)"
+        
+        // Clear text view until user taps calculate button
+        resultsTextView.text = ""
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        refreshUI()
     }
 
     override func didReceiveMemoryWarning() {
